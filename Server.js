8e799cc47b0ade683a25d57965da8ed9a7cfe69b
deeinternational01 <deeinternational01@gmail.com>
2026-09-23 12:57:57 +0545

    Update Server.js

--- Server.js
+++ Server.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err => console.error('MongoDB connection error:', err));
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`)
+})
